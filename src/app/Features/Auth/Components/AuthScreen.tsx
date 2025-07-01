c1ba9bfea5e048a8c3fbe6b1e865c44c092b5ba4
Anand Thakur <anandsingh23112005@gmail.com>
2025-07-01 10:10:46 +0530

    signin signUp ui

--- src/app/Features/Auth/Components/AuthScreen.tsx
+++ src/app/Features/Auth/Components/AuthScreen.tsx
@@ -2,15 +2,15 @@
 import React from "react";
 import { SignInFlow } from "../types";
 import { useState } from "react";
-import SignInCard from "./SignInCard";
-import SignUpCard from "./SignUpCard";
+import {SignInCard} from "./SignInCard";
+import {SignUpCard} from "./SignUpCard";
 
 const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn");
   return (
     <div className="h-full flex items-center justify-center bg-[#5c3b58]">
       <div className="md:h-auto md:w-[420px]">
-       {state === "signIn" ? <SignInCard /> : <SignUpCard />}
+       {state === "signIn" ? <SignInCard setState={setState} /> : <SignUpCard setState={setState} />}
       </div>
     </div>
   );
--- src/app/Features/Auth/Components/SignUpCard.tsx
+++ src/app/Features/Auth/Components/SignUpCard.tsx
@@ -1,11 +1,119 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Label } from "../../../Components/ui/label";
+import { Input } from "../../../Components/ui/input";
+import { cn } from "@/lib/utils";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import Image from "next/image";
+import logo from "../../../../../public/1707837044slack-icon-png.png";
+import { SignInFlow } from "../types";
 
-const SignUpCard = () => {
+interface signUpCardProps {
+  setState: (state: SignInFlow) => void;
+}
+export function SignUpCard({ setState }: signUpCardProps) {
+  const [password, SetPassword] = React.useState('');
+    const [email, SetEmail] = React.useState('');
+      const [confirmPassword, SetConfirmPassword] = React.useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
   return (
-    <div>
-      up
+    <div className="shadow-input mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
+      <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200 flex gap-2.5">
+        Let's Join Slack <Image src={logo} alt="logo" width={30} height={10} />
+      </h2>
+
+      <form className="my-8" onSubmit={handleSubmit}>
+      
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email Address</Label>
+          <Input id="email" value={email} onChange={(e) => SetEmail(e.target.value)} placeholder="projectmayhem@fc.com" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="password">Password</Label>
+          <Input value={password} onChange={(e) => SetPassword(e.target.value)} id="password" placeholder="••••••••" type="password" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="ConfirmPassword">Confirm Password</Label>
+          <Input
+          value={confirmPassword} onChange={(e) => SetConfirmPassword(e.target.value)}
+            id="ConfirmPassword"
+            placeholder="••••••••"
+            type="password"
+          />
+        </LabelInputContainer>
+
+        <button
+          className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
+          type="submit"
+        >
+          Sign up &rarr;
+          <BottomGradient />
+        </button>
+
+        <div className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
+
+        <div className="flex flex-col space-y-4">
+          <button
+            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
+            type="submit"
+          >
+            <FaGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-sm text-neutral-700 dark:text-neutral-300">
+              GitHub
+            </span>
+            <BottomGradient />
+          </button>
+          <button
+            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
+            type="submit"
+          >
+            <FcGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-sm text-neutral-700 dark:text-neutral-300">
+              Google
+            </span>
+            <BottomGradient />
+          </button>
+        </div>
+      </form>
+      <p className=" text-sm font-medium text-muted-foreground">
+        {" "}
+        Already have an account?{" "}
+        <span
+          onClick={() => setState("signUp")}
+          className=" text-sky-700 hover:underline cursor-pointer"
+        >
+          {" "}
+          SignIn
+        </span>
+      </p>
     </div>
-  )
+  );
 }
 
-export default SignUpCard
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex w-full flex-col space-y-2", className)}>
+      {children}
+    </div>
+  );
+};
