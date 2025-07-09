4dbf5d7201890b7198a10be07fc6b385f3a8038f
Anand Thakur <anandsingh23112005@gmail.com>
2025-07-09 11:03:37 +0530

    sidebarItem Done

---
+++ src/app/workspace/[workspaceId]/SidebarItem.tsx
@@ -0,0 +1,52 @@
+" use client ";
+
+import { useWorkspaceId } from "@/app/hooks/useWorkspaceId";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { cva ,type VariantProps  } from "class-variance-authority";
+import { LucideIcon } from "lucide-react";
+import Link from "next/link";
+import { IconType } from "react-icons/lib";
+
+
+const SidebarItemVerient = cva(
+  "flex items-center justify-start font-normal gap-2 rounded-md h-7 px-[18px] text-sm overflow-hidden",
+    {
+      variants: {
+        variant: {
+          default: "text-[#f9edffcc]",
+          active: "text-[#481349] bg-white/90 hover:bg-white/90",
+        },
+      },
+      defaultVariants: {
+        variant: "default",
+      },
+    }
+)
+interface SidebarItemProps {
+  id :string,
+  icon : LucideIcon | IconType,
+  label : string
+  variant? : VariantProps <typeof SidebarItemVerient>['variant'];
+
+  
+}
+const SidebarItem = ({icon : Icon , label, id ,variant }: SidebarItemProps) => {
+  const workspaceId= useWorkspaceId();
+  return (
+    <Button
+    variant={"ghost"}
+    asChild
+    size={"sm"}
+    className={ cn( SidebarItemVerient({variant}) ) }
+    
+    >
+      <Link href={`/workspace/${workspaceId}/channel/${id}`} className=" flex items-center gap-2 ">
+        <Icon className=" text-white " />
+        <span className=" text-white text-sm truncate ">{label}</span>
+      </Link>
+    </Button>
+  )
+}
+
+export default SidebarItem
--- src/app/workspace/[workspaceId]/WorkSpceSidebar.tsx
+++ src/app/workspace/[workspaceId]/WorkSpceSidebar.tsx
@@ -3,9 +3,10 @@
 import { useCuerrntMember } from "@/app/Features/members/api/useCuerrntMember";
 import { getUserWorkspace } from "@/app/Features/workSpaces/api/useGetWorkspace";
 import { useWorkspaceId } from "@/app/hooks/useWorkspaceId";
-import { AlertTriangle } from "lucide-react";
+import { AlertTriangle, MessageSquareText, SendHorizonal } from "lucide-react";
 import { TbLoader, TbLoader3 } from "react-icons/tb";
 import WorkspaceHeader from "./WorkspaceHeader";
+import SidebarItem from "./SidebarItem";
 
 const WorkSpceSidebar = () => {
   const workspaceId = useWorkspaceId();
@@ -17,25 +18,34 @@ const WorkSpceSidebar = () => {
     id: workspaceId,
   });
 
-  if(memberLoading || workspaceLoading) {
-    return <div className=" flex flex-col h-full  bg-[#5e2c5f] items-center justify-center">
-      
-      <TbLoader3 className="text-white size-5 animate-spin "/>
-    </div>;
+  if (memberLoading || workspaceLoading) {
+    return (
+      <div className=" flex flex-col h-full  bg-[#5e2c5f] items-center justify-center">
+        <TbLoader3 className="text-white size-5 animate-spin " />
+      </div>
+    );
   }
-   if(!workspace || !member) {
-    return <div className=" flex flex-col h-full space-y-1   bg-[#5e2c5f] items-center justify-center">
-      
-     <AlertTriangle className="text-white size-5 "/>
-     <p className=" text-white text-sm ">
-      workspace not found
-     </p>
-    </div>;
+  if (!workspace || !member) {
+    return (
+      <div className=" flex flex-col h-full space-y-1   bg-[#5e2c5f] items-center justify-center">
+        <AlertTriangle className="text-white size-5 " />
+        <p className=" text-white text-sm ">workspace not found</p>
+      </div>
+    );
   }
 
-  return <div className=" flex flex-col h-full  bg-[#5e2c5f]">
-    <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
-    </div>;
+  return (
+    <div className=" flex flex-col h-full  bg-[#5e2c5f]">
+      <WorkspaceHeader
+        workspace={workspace}
+        isAdmin={member.role === "admin"}
+      />
+      <div className=" flex flex-col px-2 mt-3">
+        <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
+        <SidebarItem label="Drft & Sent" icon={SendHorizonal} id="draft" />
+      </div>
+    </div>
+  );
 };
 
 export default WorkSpceSidebar;
