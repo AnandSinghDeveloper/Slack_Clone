93cffec614a23c02b2030043ebe2db14220c1102
Anand Thakur <anandsingh23112005@gmail.com>
2025-07-30 10:12:33 +0530

    leave and change role api done

--- convex/members.ts
+++ convex/members.ts
@@ -1,8 +1,7 @@
 import { v } from "convex/values";
-import { query, QueryCtx } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
 import { auth } from "./auth";
 import { Id } from "./_generated/dataModel";
-import { argv } from "process";
 
 const populateUser = async (ctx: QueryCtx, id: Id<"users">) => {
   return await ctx.db.get(id);
@@ -92,5 +91,113 @@ export const current = query({
     if (!member) return null;
 
     return member;
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("members"),
+    role: v.union(v.literal("admin"), v.literal("member")),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db.get(args.id);
+    1;
+
+    if (!member) {
+      throw new Error("Member not found");
+    }
+
+    const CurrentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_and_user_id", (q) =>
+        q.eq("workspaceId", member.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!CurrentMember || CurrentMember.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    await ctx.db.patch(args.id, { role: args.role });
+
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id("members"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db.get(args.id);
+
+    if (!member) {
+      throw new Error("Member not found");
+    }
+
+    const CurrentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_and_user_id", (q) =>
+        q.eq("workspaceId", member.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!CurrentMember) {
+      throw new Error("Unauthorized");
+    }
+
+    if (member.role === "admin") {
+      throw new Error("Admin cannot be removed");
+    }
+
+    if (CurrentMember._id || CurrentMember.role !== "admin") {
+      throw new Error(" cannot remove yourself as admin");
+    }
+
+    const [messages, reactions, conversations] = await Promise.all([
+      ctx.db
+        .query("messages")
+        .withIndex("by_member_id", (q) => q.eq("memberId", member._id))
+        .collect(),
+      ctx.db
+        .query("reactions")
+        .withIndex("by_member_id", (q) => q.eq("memberId", member._id))
+        .collect(),
+      ctx.db
+        .query("conversations")
+        .filter((q) =>
+          q.or(
+            q.eq(q.field("memberOneId"), member._id),
+            q.eq(q.field("memberTwoId"), member._id)
+          )
+        )
+        .collect(),
+    ]);
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
+    for (const reaction of reactions) {
+      await ctx.db.delete(reaction._id);
+    }
+    for (const conversation of conversations) {
+      await ctx.db.delete(conversation._id);
+    }
+
+    await ctx.db.delete(args.id);
+
+    return args.id;
   },
 });
